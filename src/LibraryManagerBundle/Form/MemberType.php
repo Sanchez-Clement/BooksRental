2b73433dc378684223d7179810c37a444a9d29bd
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-11-29 11:48:26 +0100

    LibraryManagerBundle->form-> generate MemberType.php

---
+++ src/LibraryManagerBundle/Form/MemberType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LibraryManagerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MemberType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('numberMember',    TextType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LibraryManagerBundle\Entity\Member'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'librarymanagerbundle_member';
+    }
+
+
+}
