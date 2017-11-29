2b60d2f5ab627204bcb41997bfd1fe37855085f9
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-11-29 11:18:38 +0100

    organize code in two controllers

--- src/LibraryManagerBundle/Controller/BooksController.php
+++ src/LibraryManagerBundle/Controller/BooksController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use LibraryManagerBundle\Entity\Book;
 use LibraryManagerBundle\Entity\Member;
 
-class DefaultController extends Controller
+class BooksController extends Controller
 {
     /**
      * @Route(
@@ -18,7 +18,7 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $books = $em -> getRepository(Book::class)->findAll();
-        return $this->render('LibraryManagerBundle:Default:index.html.twig',compact('books'));
+        return $this->render('LibraryManagerBundle:Books:index.html.twig',compact('books'));
     }
 
     /**
@@ -31,7 +31,7 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $book = $em -> getRepository(Book::class)->find($id);
-        return $this->render('LibraryManagerBundle:Default:viewBook.html.twig',compact('book'));
+        return $this->render('LibraryManagerBundle:Books:viewBook.html.twig',compact('book'));
 
     }
 
@@ -44,22 +44,10 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $books = $em -> getRepository(Book::class)->findByCategory($category);
-        return $this->render('LibraryManagerBundle:Default:viewCategory.html.twig',compact('books'));
+        return $this->render('LibraryManagerBundle:Books:viewCategory.html.twig',compact('books'));
 
     }
 
-    /**
-     * @Route(
-     *     path = "/members",
-     *     name = "library_members")
-     */
-    public function viewMembersAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-        $members = $em -> getRepository(Member::class)->findAll();
-        return $this->render('LibraryManagerBundle:Default:viewMembers.html.twig',compact('members'));
-    }
-
-
+    
     
 }
---
+++ src/LibraryManagerBundle/Controller/MembersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LibraryManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use LibraryManagerBundle\Entity\Book;
+use LibraryManagerBundle\Entity\Member;
+
+class MembersController extends Controller
+{
+    
+
+    /**
+     * @Route(
+     *     path = "/members",
+     *     name = "library_members")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $members = $em -> getRepository(Member::class)->findAll();
+        return $this->render('LibraryManagerBundle:Members:viewMembers.html.twig',compact('members'));
+    }
+
+       /**
+     * @Route(
+     *     path = "/members/{id}",
+     *     name = "library_thisMember",
+     *     requirements={"id": "\d+"})
+     */
+    public function viewMember($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $member = $em -> getRepository(Member::class)->find($id);
+        return $this->render('LibraryManagerBundle:Members:viewThisMember.html.twig',compact('member'));
+    }
+
+
+    
+}
