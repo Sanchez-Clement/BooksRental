e7df946efcaf2181f4e9153546f610816debee17
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-11-29 10:12:51 +0100

    LibraryManagerBundle_-> controller -> create the method viewBook()

--- src/LibraryManagerBundle/Controller/DefaultController.php
+++ src/LibraryManagerBundle/Controller/DefaultController.php
@@ -9,12 +9,28 @@ use LibraryManagerBundle\Entity\Book;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route(
+     *     path = "/",
+     *     name = "library_index")
      */
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
         $books = $em -> getRepository(Book::class)->findAll();
         return $this->render('LibraryManagerBundle:Default:index.html.twig',compact('books'));
+    }
+
+    /**
+     * @Route(
+     *     path = "/book/{id}",
+     *     name="library_viewBook" ,
+     *     requirements={"id": "\d+"})
+     */
+    public function viewBook($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $book = $em -> getRepository(Book::class)->find($id);
+        return $this->render('LibraryManagerBundle:Default:viewBook.html.twig',compact('book'));
+
     }
 }
